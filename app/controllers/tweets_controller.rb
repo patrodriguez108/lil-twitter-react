a5c06b2216a29f2e5c4d246deb939eaddbfe2719
Steven G. Harms <steven@devbootcamp.com>
2017-05-02 16:45:37 -0400

    Tweets#create: Make actually use data POSTed
    We were ignoring the content passed in and was
    instead greeking out all the fields very
    confusing.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -18,14 +18,14 @@ class TweetsController < ApplicationController
   end
 
   def create
-    tweet = Tweet.new(tweet_params)
-    tweet.content ||= Faker::Lorem.sentence
+    tweet = Tweet.new
+    tweet.content = params[:content] || Faker::Lorem.sentence
     tweet.username ||= Faker::Name.name
     tweet.handle ||= "@" + Faker::Internet.user_name
     tweet.avatar_url ||= Faker::Avatar.image(tweet.username)
     tweet.save
 
-    hashtags_names = params[:hashtags] || []
+    hashtags_names = String(params[:hashtags]).split(",") || []
     hashtags_names.each do |name|
       hashtag = Hashtag.where(name: name).first_or_create
       tweet.hashtags << hashtag
