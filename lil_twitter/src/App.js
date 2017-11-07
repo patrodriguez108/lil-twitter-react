5557ebb7a4f7a3002e2a66486eb1539f5dec3523
Patrick <pat.rodriguez108@gmail.com>
2017-11-07 17:09:56 -0600

    Add composition class

--- lil_twitter/src/App.js
+++ lil_twitter/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import SearchBox from './SearchBox'
+import Composition from './Composition'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <SearchBox />
-      
-    );
+      <div className="container">
+        <SearchBox />
+        <Composition />
+      </div>
+    )
   }
 }
 
