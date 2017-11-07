a2b6adab74b770be7f89fbc002c023f92e6c213f
Patrick <pat.rodriguez108@gmail.com>
2017-11-07 17:23:57 -0600

    Add timeline

--- lil_twitter/src/App.js
+++ lil_twitter/src/App.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react';
 import SearchBox from './SearchBox'
 import Composition from './Composition'
 import Hashtags from './Hashtags'
+import Timeline from './Timeline'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
+      <div>
         <SearchBox />
-        <Composition />
-        <Hashtags />
+        <section className="container">
+          <Composition />
+          <Hashtags />
+          <Timeline />
+        </section>
       </div>
     )
   }
