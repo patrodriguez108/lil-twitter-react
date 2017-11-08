4954059d007145b26e0d90187a6166b1dcd2ec40
Patrick <pat.rodriguez108@gmail.com>
2017-11-08 11:40:00 -0600

    Successfully parse a single tweet

--- lil_twitter/src/Timeline.js
+++ lil_twitter/src/Timeline.js
@@ -1,23 +1,43 @@
 import React, { Component } from 'react';
 import Tweet from './Tweet'
+var data = require('./recent_tweets.json')
 
 class Timeline extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			tweetContent: undefined,
+			nameOfUser: undefined,
+			handleName: undefined,
+			avatarSrc: undefined,
+			createdAt: undefined
+		}
+	}
+
+	componentDidMount() {
+		for (var i = 0; i < data.length; i++) {
+			console.log(data[i]);
+			this.setState({
+				tweetContent: data[i].content,
+				nameOfUser: data[i].username,
+				handleName: data[i].handle,
+				avatarSrc: data[i].avatar_url,
+				createdAt: data[i].created_at
+			})
+		}
+	}
 
 	render() {
 		return (
 			<section id="tweets-container">
 				<h3>Tweets</h3>
 				<ul>
-		      <Tweet />
-		      <Tweet />
-		      <Tweet />
-		      <Tweet />
-		      <Tweet />
-		      <Tweet />
-		      <Tweet />
-		      <Tweet />
-		      <Tweet />
-		      <Tweet />
+		      <Tweet tweetContent={this.state.tweetContent} nameOfUser={this.state.nameOfUser} handleName={this.state.handleName} avatarSrc={this.state.avatarSrc} createdAt={this.state.createdAt} />
+		      <Tweet tweetContent={this.state.tweetContent} nameOfUser={this.state.nameOfUser} handleName={this.state.handleName} avatarSrc={this.state.avatarSrc} createdAt={this.state.createdAt} />
+		      <Tweet tweetContent={this.state.tweetContent} nameOfUser={this.state.nameOfUser} handleName={this.state.handleName} avatarSrc={this.state.avatarSrc} createdAt={this.state.createdAt} />
+
 	      </ul>
 			</section>
 		)
--- lil_twitter/src/Tweet.js
+++ lil_twitter/src/Tweet.js
@@ -2,18 +2,25 @@ import React, { Component } from 'react';
 
 class Tweet extends Component {
 
+	constructor(props) {
+		super(props);
+		this.state = {
+
+		}
+	}
+
 	render() {
 
 		return (
 	    <li className="tweet">
-	      <img className="avatar" src="https://pbs.twimg.com/profile_images/2858734172/db7da718fd369eba6184973631d7d84a_bigger.jpeg" alt="" />
+	      <img className="avatar" src={ this.props.avatarSrc } alt="" />
 	      <div className="tweet-content">
 	        <p>
-	          <span className="full-name">Sam Samskies</span>
-	          <span className="username">@SamSamskies</span>
+	          <span className="full-name">{ this.props.nameOfUser }</span>
+	          <span className="username">{ this.props.handleName }</span>
 	          <span className="timestamp">- 6m</span>
 	        </p>
-	        <p>My latest contribution to the advancement of the human race. I present to you ver 0.0.1 of http://mileyroids.com/  #Mileyroids #miley haha :)</p>
+	        <p>{ this.props.tweetContent }</p>
 	      </div>
 	    </li>
 		)
