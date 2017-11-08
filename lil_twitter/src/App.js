e37a0dd8f0b82509d9b07d990a790df78cb83739
Patrick <pat.rodriguez108@gmail.com>
2017-11-08 10:41:30 -0600

    Add unused ajax logic in text file

--- lil_twitter/src/App.js
+++ lil_twitter/src/App.js
@@ -4,27 +4,6 @@ import Composition from './Composition'
 import Hashtags from './Hashtags'
 import Timeline from './Timeline'
 import './App.css';
-// import $ from 'jquery';
-// import { AxiosProvider, Request, Get, Delete, Head, Post, Put, Patch, withAxios } from 'react-axios'
-// import axios from 'axios'
-
-
-
-// $(document).ready(function() {
-//   $.ajax({
-//     method: 'GET',
-//     url: 'http://localhost:3000/tweets/recent'
-//   }).done(function(response) {
-//     console.log(response)
-//   })
-//   axios.get('/tweets/recent')
-//   .then(function (response) {
-//     console.log(response);
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   });
-// })
 
 class App extends Component {
   render() {
